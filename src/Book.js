32c450684596e0c3c0fbf9be28902e61ec859fd8
Choplin <matthieu.choplin@made.com>
2019-01-29 19:39:14 +0000

    Convert Book Component into a Functional Component

--- src/Book.js
+++ src/Book.js
@@ -1,36 +1,34 @@
 import React from "react";
 
-class Book extends React.Component {
-  render() {
-    const style = {
-      width: 128,
-      height: 193,
-      backgroundImage:  this.props.book.imageLinks ? "url(" + this.props.book.imageLinks.thumbnail + ")" : "#"
-    };
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={style} />
-          <div className="book-shelf-changer">
-            <select
-              onChange={e => this.props.onChangeShelf(this.props.book.id, e)}
-              value={this.props.book.shelf}
-            >
-              <option value="move" disabled>
-                Move to...
-              </option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+function Book(props) {
+  const style = {
+    width: 128,
+    height: 193,
+    backgroundImage: props.book.imageLinks ? "url(" + props.book.imageLinks.thumbnail + ")" : "#"
+  };
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={style}/>
+        <div className="book-shelf-changer">
+          <select
+            onChange={e => props.onChangeShelf(props.book.id, e)}
+            value={props.book.shelf}
+          >
+            <option value="move" disabled>
+              Move to...
+            </option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
       </div>
-    );
-  }
+      <div className="book-title">{props.book.title}</div>
+      <div className="book-authors">{props.book.authors}</div>
+    </div>
+  );
 }
 
 export default Book;
